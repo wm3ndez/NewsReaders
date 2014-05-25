1c001e995077693272deb34c40d6814ce93ef1ee
Williams Mendez <wmendez27@gmail.com>
2014-05-25 10:41:07 -0400

    Not displaying Portada and Cinema, always empty.

--- diariolibre/src/main/java/com/wmendez/diariolibre/DiarioLibreApp.java
+++ diariolibre/src/main/java/com/wmendez/diariolibre/DiarioLibreApp.java
@@ -13,12 +13,12 @@ public class DiarioLibreApp extends Application {
 
     @Override
     public void onCreate() {
-        addItem(new Feeds.FeedItem("Portada", "http://www.diariolibre.com/rss/diariolibre/", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
+//        addItem(new Feeds.FeedItem("Portada", "http://www.diariolibre.com/rss/diariolibre/", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
+//        addItem(new Feeds.FeedItem("Cinema", "http://www.diariolibre.com/rss/?id=72", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
         addItem(new Feeds.FeedItem("Noticias", "http://www.diariolibre.com/rss/noticias/", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
         addItem(new Feeds.FeedItem("Economía", "http://www.diariolibre.com/rss/?id=10", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
         addItem(new Feeds.FeedItem("Actualidad", "http://www.diariolibre.com/rss/?id=64", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
         addItem(new Feeds.FeedItem("Revista", "http://www.diariolibre.com/rss/revista/", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
-        addItem(new Feeds.FeedItem("Cinema", "http://www.diariolibre.com/rss/?id=72", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
         addItem(new Feeds.FeedItem("Deportes", "http://www.diariolibre.com/rss/deportes/", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
         addItem(new Feeds.FeedItem("Opinión", "http://www.diariolibre.com/rss/opinion/", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
         addItem(new Feeds.FeedItem("Ecos", "http://www.diariolibre.com/rss/ecos/", com.wmendez.newsreader.lib.R.drawable.ic_action_web_site));
@@ -35,7 +35,7 @@ public class DiarioLibreApp extends Application {
             public String getHtml(Document doc) {
                 try {
                     return doc.getElementById("newstext").html();
-                }catch (NullPointerException e){
+                } catch (NullPointerException e) {
                     //No News
                     return "<h4 style='text-align:center;'>Oh oh! Esta noticia no existe.</h4>";
                 }
