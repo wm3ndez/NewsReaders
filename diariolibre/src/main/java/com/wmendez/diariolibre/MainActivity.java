d974d66ded2437a03338674936dbf012d33ea5f6
Williams Mendez <wmendez27@gmail.com>
2014-05-27 15:25:36 -0400

    Added BugSense to "Diario Libre" + minor changes

--- diariolibre/src/main/java/com/wmendez/diariolibre/MainActivity.java
+++ diariolibre/src/main/java/com/wmendez/diariolibre/MainActivity.java
@@ -1,10 +1,17 @@
 package com.wmendez.diariolibre;
 
 
+import android.os.Bundle;
+
+import com.bugsense.trace.BugSenseHandler;
 import com.wmendez.newsreader.lib.ui.FeedCategoryListActivity;
 
 
 public class MainActivity extends FeedCategoryListActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        BugSenseHandler.initAndStartSession(MainActivity.this, getString(R.string.bugsense_key));
+        super.onCreate(savedInstanceState);
+    }
 
 }
