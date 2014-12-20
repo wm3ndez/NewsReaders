1c358d6cf5c10542920873abf227787df23a4b03
Williams Mendez <wmendez27@gmail.com>
2014-12-20 00:56:39 -0400

    Implementing SlidingTabs. Closing issue #6

--- newsreader/src/main/java/com/wmendez/newsreader/lib/ui/FeedCategoryListActivity.java
+++ newsreader/src/main/java/com/wmendez/newsreader/lib/ui/FeedCategoryListActivity.java
@@ -2,24 +2,18 @@ package com.wmendez.newsreader.lib.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 
 import com.wmendez.newsreader.lib.R;
-import com.wmendez.newsreader.lib.event.CategoryItemSelectedEvent;
 import com.wmendez.newsreader.lib.event.NewsItemSelectedEvent;
-import com.wmendez.newsreader.lib.helpers.Feeds;
 
 import de.greenrobot.event.EventBus;
 
 
 /**
- * An activity representing a list of Items. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link com.wmendez.newsreader.lib.ui.NewsActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
+ * An activity representing a list of Items.
  * <p/>
  * The activity makes heavy use of fragments. The list of items is a
  * {@link com.wmendez.newsreader.lib.ui.FeedListFragment}.
@@ -27,69 +21,26 @@ import de.greenrobot.event.EventBus;
  */
 public class FeedCategoryListActivity extends ActionBarActivity {
 
-    private static final String TAG = FeedCategoryListActivity.class.getCanonicalName();
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private boolean mTwoPane;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_list);
+        setContentView(R.layout.activity_main);
 
-        if (findViewById(R.id.feed_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-large and
-            // res/values-sw600dp). If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-            // In two-pane mode, list items should be given the
-            // 'activated' state when touched.
-            ((FeedCategoryListFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.item_list))
-                    .setActivateOnItemClick(true);
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            SlidingTabsColorsFragment fragment = new SlidingTabsColorsFragment();
+            transaction.replace(R.id.content_fragment, fragment);
+            transaction.commit();
         }
+
         EventBus.getDefault().register(this);
-
     }
 
-
-    public void onEvent(CategoryItemSelectedEvent event) {
-        Feeds.FeedItem item = event.getItem();
-        if (mTwoPane) {
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            Bundle arguments = new Bundle();
-            arguments.putString("title", item.title);
-            arguments.putString("uri", item.uri);
-            arguments.putInt("resource", item.resource);
-            FeedListFragment fragment = new FeedListFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.feed_detail_container, fragment)
-                    .commit();
-
-        } else {
-            // In single-pane mode, simply start the detail activity
-            // for the selected item ID.
-            Intent detailIntent = new Intent(this, FeedListActivity.class);
-            detailIntent.putExtra("title", item.title);
-            detailIntent.putExtra("uri", item.uri);
-            detailIntent.putExtra("resource", item.resource);
-            startActivity(detailIntent);
-        }
-    }
 
     public void onEvent(NewsItemSelectedEvent event) {
-        //Do not open the News if it's One-Pane layout, because it was opened at FeedListActivity
-        if (!mTwoPane)
-            return;
-        Log.i(TAG, "Clicked!!!");
         Intent intent = new Intent(this, NewsActivity.class);
         intent.putExtra("news", event.getEntry());
         startActivity(intent);
---
+++ newsreader/src/main/java/com/wmendez/newsreader/lib/ui/SlidingTabsColorsFragment.java
@@ -0,0 +1,205 @@
+package com.wmendez.newsreader.lib.ui;
+/*
+* Copyright 2013 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wmendez.newsreader.lib.R;
+import com.wmendez.newsreader.lib.helpers.Feeds;
+import com.wmendez.newsreader.lib.ui.views.SlidingTabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A basic sample which shows how to use {@link com.wmendez.newsreader.lib.ui.views.SlidingTabLayout}
+ * to display a custom {@link ViewPager} title strip which gives continuous feedback to the user
+ * when scrolling.
+ */
+public class SlidingTabsColorsFragment extends Fragment {
+    /**
+     * This class represents a tab to be displayed by {@link ViewPager} and it's associated
+     * {@link SlidingTabLayout}.
+     */
+    static class PagerItem {
+        private final int mIndicatorColor;
+        private final int mDividerColor;
+        private final Feeds.FeedItem mItem;
+
+        PagerItem(Feeds.FeedItem item, int indicatorColor, int dividerColor) {
+            mItem = item;
+            mIndicatorColor = indicatorColor;
+            mDividerColor = dividerColor;
+
+        }
+
+        /**
+         * @return A new {@link Fragment} to be displayed by a {@link ViewPager}
+         */
+        Fragment createFragment() {
+            Bundle arguments = new Bundle();
+            arguments.putString("title", mItem.title);
+            arguments.putString("uri", mItem.uri);
+            arguments.putInt("resource", mItem.resource);
+            FeedListFragment fragment = new FeedListFragment();
+            fragment.setArguments(arguments);
+            return fragment;
+        }
+
+        /**
+         * @return the title which represents this tab. In this sample this is used directly by
+         * {@link android.support.v4.view.PagerAdapter#getPageTitle(int)}
+         */
+        CharSequence getTitle() {
+            return mItem.title;
+        }
+
+        /**
+         * @return the color to be used for indicator on the {@link SlidingTabLayout}
+         */
+        int getIndicatorColor() {
+            return mIndicatorColor;
+        }
+
+        /**
+         * @return the color to be used for right divider on the {@link SlidingTabLayout}
+         */
+        int getDividerColor() {
+            return mDividerColor;
+        }
+    }
+
+    /**
+     * List of {@link com.wmendez.newsreader.lib.ui.SlidingTabsColorsFragment.PagerItem} which represent this sample's tabs.
+     */
+    private List<PagerItem> mTabs = new ArrayList<>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /**
+         * Populate our tab list with tabs. Each item contains a title, indicator color and divider
+         * color, which are used by {@link SlidingTabLayout}.
+         */
+
+        for (Feeds.FeedItem item : Feeds.ITEMS) {
+            mTabs.add(new PagerItem(
+                    item,
+                    Color.WHITE, // Indicator color
+                    Color.WHITE // Divider color
+            ));
+        }
+    }
+
+    /**
+     * Inflates the {@link View} which will be displayed by this {@link Fragment}, from the app's
+     * resources.
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news_list, container, false);
+    }
+
+    /**
+     * This is called after the {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)} has finished.
+     * Here we can pick out the {@link View}s we need to configure from the content view.
+     * <p/>
+     * We set the {@link ViewPager}'s adapter to be an instance of
+     * {@link com.wmendez.newsreader.lib.ui.SlidingTabsColorsFragment.NewsFragmentPagerAdapter}. The {@link SlidingTabLayout} is then given the
+     * {@link ViewPager} so that it can populate itself.
+     *
+     * @param view View created in {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
+     */
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        /*
+      A {@link ViewPager} which will be used in conjunction with the {@link SlidingTabLayout} above.
+     */
+        ViewPager mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new NewsFragmentPagerAdapter(getChildFragmentManager()));
+        // Give the SlidingTabLayout the ViewPager, this must be done AFTER the ViewPager has had
+        // it's PagerAdapter set.
+        /*
+      A custom {@link ViewPager} title strip which looks much like Tabs present in Android v4.0 and
+      above, but is designed to give continuous feedback to the user when scrolling.
+     */
+        SlidingTabLayout mSlidingTabLayout = (SlidingTabLayout) view.findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setViewPager(mViewPager);
+        // Set a TabColorizer to customize the indicator and divider colors. Here we just retrieve
+        // the tab at the position, and return it's set color
+        mSlidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return mTabs.get(position).getIndicatorColor();
+            }
+
+            @Override
+            public int getDividerColor(int position) {
+                return mTabs.get(position).getDividerColor();
+            }
+        });
+    }
+
+    /**
+     * The {@link FragmentPagerAdapter} used to display pages in this sample. The individual pages
+     * are instances of {@link ContentFragment} which just display three lines of text. Each page is
+     * created by the relevant {@link com.wmendez.newsreader.lib.ui.SlidingTabsColorsFragment.PagerItem} for the requested position.
+     * <p/>
+     * The important section of this class is the {@link #getPageTitle(int)} method which controls
+     * what is displayed in the {@link SlidingTabLayout}.
+     */
+    class NewsFragmentPagerAdapter extends FragmentPagerAdapter {
+        NewsFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Return the {@link android.support.v4.app.Fragment} to be displayed at {@code position}.
+         * <p/>
+         * Here we return the value returned from {@link com.wmendez.newsreader.lib.ui.SlidingTabsColorsFragment.PagerItem#createFragment()}.
+         */
+        @Override
+        public Fragment getItem(int i) {
+            return mTabs.get(i).createFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return mTabs.size();
+        }
+
+        /**
+         * Return the title of the item at {@code position}. This is important as what this method
+         * returns is what is displayed in the {@link SlidingTabLayout}.
+         * <p/>
+         * Here we return the value returned from {@link com.wmendez.newsreader.lib.ui.SlidingTabsColorsFragment.PagerItem#getTitle()}.
+         */
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTabs.get(position).getTitle();
+        }
+    }
+}
