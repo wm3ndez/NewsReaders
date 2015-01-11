8a7757612642e44e0f2898dd15e22ba9573885f4
Williams Mendez <wmendez27@gmail.com>
2015-01-11 12:11:04 -0400

    Moving to crashlytics

--- newsreader/src/main/java/com/wmendez/newsreader/lib/NewsApp.java
+++ newsreader/src/main/java/com/wmendez/newsreader/lib/NewsApp.java
@@ -2,7 +2,10 @@ package com.wmendez.newsreader.lib;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.wmendez.newsreader.lib.accounts.AccountUtils;
+
+import io.fabric.sdk.android.Fabric;
 
 public class NewsApp extends Application {
 
@@ -10,5 +13,6 @@ public class NewsApp extends Application {
     public void onCreate() {
         if (!AccountUtils.accountExists(getApplicationContext()))
             AccountUtils.createAccount(getApplicationContext());
+        Fabric.with(this, new Crashlytics());
     }
 }
