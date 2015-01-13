df920f4f2a1faaf074dbded79bc9f7ef6e17ca5d
Williams Mendez <wmendez27@gmail.com>
2015-01-13 17:38:03 -0400

    Removing Mint from diariolibre

--- newsreader/src/diarioLibre/java/com/wmendez/diariolibre/MainActivity.java
+++ newsreader/src/diarioLibre/java/com/wmendez/diariolibre/MainActivity.java
@@ -3,15 +3,12 @@ package com.wmendez.diariolibre;
 
 import android.os.Bundle;
 
-import com.splunk.mint.Mint;
-import com.wmendez.newsreader.lib.R;
 import com.wmendez.newsreader.lib.ui.FeedCategoryListActivity;
 
 
 public class MainActivity extends FeedCategoryListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Mint.initAndStartSession(MainActivity.this, getString(R.string.bugsense_key));
         super.onCreate(savedInstanceState);
     }
 
