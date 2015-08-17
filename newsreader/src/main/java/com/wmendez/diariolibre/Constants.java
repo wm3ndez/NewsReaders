0e7d4c7a192794d6869450a31e8787a35730803f
Williams Mendez <wmendez27@gmail.com>
2015-08-17 15:45:48 -0400

    New Logo, New Name

--- newsreader/src/main/java/com/wmendez/diariolibre/Constants.java
+++ newsreader/src/main/java/com/wmendez/diariolibre/Constants.java
@@ -1,7 +1,7 @@
 package com.wmendez.diariolibre;
 
 public class Constants {
-    public static String ACCOUNT_NAME = "News Reader";
+    public static String ACCOUNT_NAME = "Periodicos RD";
     public static String ACCOUNT_TYPE = "com.wmendez.diariolibre.syncadapter";
     public static String CONTENT_AUTHORITY = ACCOUNT_TYPE;
     public static final int UPDATE_FREQUENCY = 2 * 60 * 60; // Every two hours
