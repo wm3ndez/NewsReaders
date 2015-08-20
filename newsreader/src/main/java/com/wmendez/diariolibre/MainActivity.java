4f3e4c1e8648765cb4ab6c7928a1cf56652758e7
Williams Mendez <wmendez27@gmail.com>
2015-08-19 22:20:26 -0400

    reverting libs + minor changes

--- newsreader/src/main/java/com/wmendez/diariolibre/MainActivity.java
+++ newsreader/src/main/java/com/wmendez/diariolibre/MainActivity.java
@@ -1,15 +1,9 @@
 package com.wmendez.diariolibre;
 
 
-import android.os.Bundle;
-
 import com.wmendez.newsreader.lib.ui.FeedCategoryListActivity;
 
 
 public class MainActivity extends FeedCategoryListActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
 }
